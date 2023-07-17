b5ba5f39810cff0f8e40f8ba48e89eaaeede442e
Abdullah9001 <abdullahjubayer44@gmail.com>
2023-07-18 01:11:01 +0600

    start a new project

---
+++ src/App.vue
@@ -0,0 +1,48 @@
+<script setup>
+import { ref } from "vue";
+const count = ref(0);
+
+const increment = () => {
+  count.value = count.value + 1;
+};
+const decrement = () => {
+  count.value = count.value - 1;
+};
+</script>
+
+<template>
+  <main>
+    <div>
+      <h4>This current counter is..</h4>
+      <h1>{{ count }}</h1>
+      <button @click="decrement">-</button>
+      <button @click="increment">+</button>
+    </div>
+  </main>
+</template>
+
+<style lang="scss" scoped>
+main {
+  background-color: aliceblue;
+  height: 100vh;
+  width: 100vw;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  div {
+    text-align: center;
+    h4 {
+      font-size: 2rem;
+    }
+    h1 {
+      font-size: 3rem;
+    }
+
+    button {
+      padding: 1px 25px;
+      margin: 5px;
+      font-size: 40px;
+    }
+  }
+}
+</style>
