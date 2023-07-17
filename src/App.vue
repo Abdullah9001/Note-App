c3e640f8e35287f7ecc026f34dfa2029a259a51b
Abdullah9001 <abdullahjubayer44@gmail.com>
2023-07-18 02:01:47 +0600

    done header section style

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,76 @@
-<script setup></script>
+<script setup>
+import { Icon } from "@iconify/vue";
+</script>
 
 <template>
-  <main class="container">
-    <div class="header">
-      <h1>Notes</h1>
-      <h1>+</h1>
-    </div>
-    <div class="cards-containet">
-      <div class="card">
-        <p class="main-text">this is my new note</p>
-        <p class="date">the date</p>
+  <main>
+    <div class="container">
+      <header>
+        <h1>Notes</h1>
+        <button><Icon icon="ic:outline-plus" width="25" /></button>
+      </header>
+      <div class="cards-container">
+        <div class="card">
+          <p class="main-text">this is my new note</p>
+          <p class="date">the date</p>
+        </div>
       </div>
     </div>
   </main>
 </template>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+main {
+  height: 100vh;
+  width: 100vw;
+  background-color: #dfe4ea;
+
+  .container {
+    max-width: 1000px;
+    padding: 10px;
+    margin: 0 auto;
+
+    header {
+      display: flex;
+      align-items: center;
+      justify-content: space-between;
+
+      h1 {
+        color: #0a3d62;
+        font-size: 75px;
+        font-family: Arial, Helvetica, sans-serif;
+      }
+      button {
+        width: 50px;
+        height: 50px;
+        border-radius: 100%;
+        border: none;
+        font-size: 30px;
+        background-color: #0a3d62;
+        color: #fff;
+        cursor: pointer;
+      }
+    }
+    .cards-container {
+      display: flex;
+      flex-wrap: wrap;
+      .card {
+        width: 225px;
+        height: 225px;
+        display: flex;
+        flex-direction: column;
+        justify-content: space-between;
+        background-color: #0a3d62;
+        border-radius: 15px;
+        margin-right: 20px;
+        margin-bottom: 20px;
+        padding: 10px;
+        p {
+          color: #fff;
+          font-size: 12px;
+        }
+      }
+    }
+  }
+}
+</style>
